575b23b9dbbdaf8c4a5438590f0d828d3830d343
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-29 17:09:01 +0200

    mod home.. welcome message

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container py-5">
-        <h1>Bentornato {{Auth::user()->name}}</h1>
+        <h1>Bentornato Admin</h1>
         <p>cosa vuoi fare oggi?</p>
 
         <a href="{{route('admin.projects.index')}}" class="btn btn-primary my-5 mx-2">Vai ai progetti</a>
